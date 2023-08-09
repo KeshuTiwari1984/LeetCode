cfefc8506238892778898876a8b175c54202b005
KeshuTiwari1984 <76833268+KeshuTiwari1984@users.noreply.github.com>
2023-08-09 16:05:20 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.cpp
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.cpp
@@ -10,15 +10,16 @@ using namespace std;
 class Solution {
   public:
     int getMinDiff(int arr[], int n, int k) {
+        
         // we'll iterate over each element and consider it as the max element
         sort(arr, arr+n);
-        // for (int i =0; i<n; i++) cout << arr[i] << ' '; cout << endl;
+        
         int ans = 1e9;
         for (int i=n-1; i>=0; i--)
         {
             // check if it can be maximum or not
             // arr[i] + k
-            if (i == n-1 or (arr[n-1]-k >= 0 and arr[n - 1] - k <= arr[i] + k))
+            if (i == n - 1 or (arr[n-1] - k >= 0 and arr[n - 1] - k <= arr[i] + k))
             {
                 int maxn = 0;
                 if (i != 0)
@@ -26,37 +27,44 @@ class Solution {
                     int cur = arr[i] + k - arr[0] - k;
                     maxn = max(maxn, cur);
                 }
-                // cout << i << ' ' << maxn << endl;
+               
                 if (i != n - 1)
                 {
                     if (arr[i+1]-k >= 0)
-                        maxn = max(maxn, abs(arr[i]+k - arr[i+1] + k));
+                        maxn = max(maxn, abs(arr[i] + k - arr[i+1] + k));
                     else
                     {
                         // arr[i] + k cannot be maxima
                         break;
                     }
                 }
-                /                ans = min(ans, maxn);
+               
+                ans = min(ans, maxn);
             }
         }
         
         int maxn = 0;
-        if (arr[n-1]-k >= 0)
+        
+        // check arr[i] - k, so only i = n-1 is possible
+        // taki arr[i] is the maxima
+        if (arr[n-1] - k >= 0)
         {
             for (int i=0; i<n-1; i++)
             {
                 int mn = 1e9;
+                
+                // do arr[i] + k, if u can i.e., arr[n-1] - k is still maxima
+                if (arr[i] + k <= arr[n-1] - k)
+                    mn = min(mn, abs(arr[n-1] - k - arr[i] - k));
+                    
                 if (arr[i]-k >= 0)
                     mn = min(mn, abs(arr[n-1] - k - arr[i] + k));
                 else
                 {
+                    // since, we couldn't do (-k), so we've got to do (+k)
                     // arr[n-1] - k is no longer maxima
                     if (arr[i] + k > arr[n-1] - k) return ans;
                 }
-                if (arr[i] + k <= arr[n-1] - k)
-                    mn = min(mn, abs(arr[n-1] - k - arr[i] - k));
-                
                 maxn = max(maxn, mn);
             }
             ans = min(ans, maxn);
